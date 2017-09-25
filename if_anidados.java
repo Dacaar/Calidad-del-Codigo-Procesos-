6e7b6666bf47f07ad0f9ee1c2ffcf4a87dc43eaf
Dacaar <davison.c.a@gmail.com>
2017-09-25 20:22:47 +0200

    Create if_anidados.java

---
+++ if_anidados.java
@@ -0,0 +1,21 @@
+public class Ifanidados{
+
+    public static void main (String [] args){
+        int i = 0;
+        int j = 1;
+        int k = 2;
+        int f = 20;
+        
+        while (i=0){
+            if (j<f){
+                if (k>j){
+                    if (k < f){
+                        k++;
+                    }
+                }
+            }
+            j++;
+         }
+     }
+ }
+             
